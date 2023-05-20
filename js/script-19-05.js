75edd916bb99081b53f3d5cd49fc183c5b0665fc
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-05-20 11:01:41 +0300

    19.05 up

---
+++ js/script-19-05.js
@@ -0,0 +1,105 @@
+//=====================================================
+// Напишіть цикл for, який виконуватиме ітерацію від 0 до 15. Для кожної ітерації він перевірятиме, парне чи непарне поточне число, і виводитиме повідомлення на екран.
+
+// for (let i = 0; i < 15; i += 1){
+//     if (i % 2 === 0) {
+//         console.log(i, "парне");
+//     }
+//     else{
+//         console.log(i, "непарне");
+//     }
+// }
+
+// for (let i = 0; i <= 15; i += 1){
+//     switch (i % 2) {
+//         case 0:
+//             console.log(i, "парне");
+//             break;
+//         default:
+//            console.log(i, "непарне");
+//     }
+// }
+
+// for (let i = 0; i < 15; i += 1){
+//     i % 2 === 0 ? console.log(i, "парне"): console.log(i, "непарне");
+// }
+
+// for (let i = 0; i < 15; i += 1){
+//     console.log(i, i % 2 === 0 ? "парне": "непарне");
+// }
+
+//=====================================================
+
+// Потрібно створити світлофор за допомогою switch()
+// Користувач вводить в prompt() колір який він бачить
+// В результаті отримує меседж з дією яку повинен виконати
+// Світлофор може мати 4 стани green (GO), yellow (READY),red (STOP), будь що інше означає що сфітлофор не працює (BE CAREFUL)
+
+// let message = prompt();
+// if (typeof message === 'string') {
+//   //console.log(typeof message);
+//   message = message.toLowerCase();
+// }
+// switch (message) {
+//   case 'green':
+//     console.log('GO');
+//     break;
+//   case 'yellow':
+//     console.log('READY');
+//     break;
+//   case 'red':
+//     console.log('STOP');
+//     break;
+//   default:
+//     console.log('BE CAREFUL');
+//     break;
+// }
+
+// //console.log(message);
+
+//=====================================================
+
+// //Створити функцію яка буде рахувати за скільки днів равлик зможе виповзти з колодязя, функція приймає 1 параметр.
+// //1 глибина колодязя (depth)
+
+// //Функція повертає кількість днів
+
+// // Умови:
+// // вдень Равлик проповзає на 7 м вгору, а за ніч опускається на 2 м вниз
+// // використовувати цикл while()
+
+// function calcDays(depth) {
+//   // step 1
+//   const daySpeed = 7;
+//   const naghtSpeed = 2;
+//   let days = 0;
+//   let total = 0;
+//   while (total < depth) {
+//     days += 1;
+//     total += daySpeed;
+//     if (total < depth) {
+//       total -= naghtSpeed;
+//     }
+//   }
+//   console.log(days);
+// }
+// calcDays(42); //виповзе за 8 днів
+// calcDays(17); //виповзе за 3 дні
+// calcDays(18); //виповзе за 4 дні
+
+//=====================================================
+
+// Просумувати всі елемнти рядку, якщо їх можливо привести до числа
+//const str = '357g235';
+
+// const str = '357g235';
+// let TotalElements = 0;
+
+// for (i = 0; i < str.length; i += 1) {
+//   const num = Number(str[i]);
+
+//   if (!isNaN(num)) {
+//     TotalElements += num;
+//   }
+// }
+// console.log(TotalElements);
--- js/script.js
+++ js/script.js
@@ -1,105 +1,157 @@
-//=====================================================
-// Напишіть цикл for, який виконуватиме ітерацію від 0 до 15. Для кожної ітерації він перевірятиме, парне чи непарне поточне число, і виводитиме повідомлення на екран.
-
-// for (let i = 0; i < 15; i += 1){
-//     if (i % 2 === 0) {
-//         console.log(i, "парне");
-//     }
-//     else{
-//         console.log(i, "непарне");
-//     }
+// Напишіть код, який очищає масив від небажаних значень, таких як false undefined, пусті рядки, нуль, null.
+// function filterArray(arr){
+//     ...
 // }
+// console.log(filterArray([0, 1, false, 2, undefined, '', 3, null])) // [1, 2, 3]
+// console.log(filterArray([NaN, 0, 15, false, -22, '',undefined, 47, null])) // [15, -22, 47]
 
-// for (let i = 0; i <= 15; i += 1){
-//     switch (i % 2) {
-//         case 0:
-//             console.log(i, "парне");
-//             break;
-//         default:
-//            console.log(i, "непарне");
-//     }
-// }
+// false, 0, NaN, '', undefined, null;
 
-// for (let i = 0; i < 15; i += 1){
-//     i % 2 === 0 ? console.log(i, "парне"): console.log(i, "непарне");
-// }
-
-// for (let i = 0; i < 15; i += 1){
-//     console.log(i, i % 2 === 0 ? "парне": "непарне");
-// }
-
-//=====================================================
-
-// Потрібно створити світлофор за допомогою switch()
-// Користувач вводить в prompt() колір який він бачить
-// В результаті отримує меседж з дією яку повинен виконати
-// Світлофор може мати 4 стани green (GO), yellow (READY),red (STOP), будь що інше означає що сфітлофор не працює (BE CAREFUL)
-
-// let message = prompt();
-// if (typeof message === 'string') {
-//   //console.log(typeof message);
-//   message = message.toLowerCase();
-// }
-// switch (message) {
-//   case 'green':
-//     console.log('GO');
-//     break;
-//   case 'yellow':
-//     console.log('READY');
-//     break;
-//   case 'red':
-//     console.log('STOP');
-//     break;
-//   default:
-//     console.log('BE CAREFUL');
-//     break;
-// }
-
-// //console.log(message);
-
-//=====================================================
-
-// //Створити функцію яка буде рахувати за скільки днів равлик зможе виповзти з колодязя, функція приймає 1 параметр.
-// //1 глибина колодязя (depth)
-
-// //Функція повертає кількість днів
-
-// // Умови:
-// // вдень Равлик проповзає на 7 м вгору, а за ніч опускається на 2 м вниз
-// // використовувати цикл while()
-
-// function calcDays(depth) {
-//   // step 1
-//   const daySpeed = 7;
-//   const naghtSpeed = 2;
-//   let days = 0;
-//   let total = 0;
-//   while (total < depth) {
-//     days += 1;
-//     total += daySpeed;
-//     if (total < depth) {
-//       total -= naghtSpeed;
+// function filterArray(arr) {
+//   const result = [];
+//   for (let item of arr) {
+//     if (!!item) {
+//       result.push(item);
 //     }
 //   }
-//   console.log(days);
+//   return result;
 // }
-// calcDays(42); //виповзе за 8 днів
-// calcDays(17); //виповзе за 3 дні
-// calcDays(18); //виповзе за 4 дні
+// const items = [0, 1, false, 2, undefined, '', 3, null];
+// filterArray([0, 1, false, 2, undefined, '', 3, null]);
 
-//=====================================================
+// console.log(filterArray(items)); // [1, 2, 3]
+// console.log(filterArray([NaN, 0, 15, false, -22, '', undefined, 47, null])); // [15, -22, 47]
 
-// Просумувати всі елемнти рядку, якщо їх можливо привести до числа
-//const str = '357g235';
+//======================
 
-const str = '357g235';
-let TotalElements = 0;
+//Напишіть код, який поверне суму рядка
+// function digitSum(data){
+//     ...
+// }
+// console.log(digitSum('4367')) // 20
+// console.log(digitSum('56349')) // 27
 
-for (i = 0; i < str.length; i += 1) {
-  const num = Number(str[i]);
+// function digitSum(data){
+//     const numbers = data.split("");
+//     let num = 0;
+//     for (let elem of numbers) {
+//          num += Number(elem);
+//     }
+//     return num;
 
-  if (!isNaN(num)) {
-    TotalElements += num;
+// }
+// console.log(digitSum('4367')) // 20
+// console.log(digitSum('56349')) // 27
+
+//======================
+
+// Потрібно мутувати масив так щоб залишились тільки (імена) елементи типу String.
+//0     1    2     3    4     5
+// const names = ['Anna', 3, 'Roma', 17, 25, 'Kate', 14, 'Oleg'];
+// for (let i = names.length - 1; i >= 0; i -= 1) {
+//   if (typeof names[i] !== 'string') {
+//     names.splice(i, 1);
+//   }
+// }
+// // Варіант №2
+// // for (let i = 0; i < names.length; i += 1){
+// //     if (typeof names[i] !== 'string') {
+// //         names.splice(i, 1);
+// //         i -= 1;  // зменшуємо індекс, бо змінилася індексація в масиві
+// //     }
+// // }
+// console.log(names);
+
+//======================
+
+// Створити код який буде повертати масив з жіночими іменамиі
+// const users = [
+//   'Artem',
+//   'Anna',
+//   'Larisa',
+//   'Maksim',
+//   'Svetlana',
+//   'David',
+//   'Roman',
+//   'Olga',
+// ];
+// const men = ['Artem', 'Maksim', 'David', 'Roman'];
+
+//======================
+
+// Створити код який буде повертати масив з жіночими іменами
+
+// const users = [
+//   'Artem',
+//   'Anna',
+//   'Larisa',
+//   'Maksim',
+//   'Svetlana',
+//   'David',
+//   'Roman',
+//   'Olga',
+// ];
+// const men = ['Artem', 'Maksim', 'David', 'Roman'];
+
+// const women = [];
+
+// for (let el of users) {
+//   if (!men.includes(el)) {
+//     console.log(el);
+//     women.push(el);
+//   }
+// }
+// console.log(women);
+
+//======================
+
+// //Створіть функцію, яка приймає 1 параметр
+// //1 масив імен користувачів, які поставили лайки.
+// //Функція повертає меседж, як вказано у прикладах:
+// //Використовувати switch()
+
+// function stringBuilder(users) {
+//   let message;
+//   switch (users.length) {
+//     case 0:
+//       message = 'no one likes this';
+//       break;
+//     case 1:
+//       message = `${users[0]} likes this `;
+//       break;
+//     case 2:
+//       message = `${users[0]} and ${users[1]} likes this `;
+//       break;
+//     case 3:
+//       message = `${users[0]}, ${users[1]} and ${users[2]} likes this `;
+//       break;
+//     default:
+//       message = `${users[0]}, ${users[1]} and ${
+//         users.length - 2
+//       } others like this`;
+//   }
+//   return message;
+// }
+
+// console.log(stringBuilder([])); //"no one likes this";
+// console.log(stringBuilder(['Peter'])); //"Peter likes this";
+// console.log(stringBuilder(['Max', 'Alex'])); //"Jacob and Alex like this";
+// console.log(stringBuilder(['Max', 'John', 'Mark'])); //"Max, John and Mark like this";
+// console.log(stringBuilder(['Alex', 'Jacob', 'Mark', 'Max', 'Igor', 'Leo'])); //"Alex, Jacob and 2 others like this;
+
+function filterArray(numbers, value) {
+  // Change code below this line
+  const result = [];
+  for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i]);
+    if (numbers[i] > value) {
+      console.log(numbers[i]);
+      result.push(numbers[i]);
+      console.log(result);
+    }
   }
+  // Change code above this line
+  return result;
 }
-console.log(TotalElements);
+
+filterArray([1, 2, 3, 4, 5], 3);

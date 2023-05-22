a53924a30e257f63c42f187c5a32d99df0566ac1
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-05-22 17:51:54 +0300

    22.05

---
+++ js/script-20-05.js
@@ -0,0 +1,157 @@
+// Напишіть код, який очищає масив від небажаних значень, таких як false undefined, пусті рядки, нуль, null.
+// function filterArray(arr){
+//     ...
+// }
+// console.log(filterArray([0, 1, false, 2, undefined, '', 3, null])) // [1, 2, 3]
+// console.log(filterArray([NaN, 0, 15, false, -22, '',undefined, 47, null])) // [15, -22, 47]
+
+// false, 0, NaN, '', undefined, null;
+
+// function filterArray(arr) {
+//   const result = [];
+//   for (let item of arr) {
+//     if (!!item) {
+//       result.push(item);
+//     }
+//   }
+//   return result;
+// }
+// const items = [0, 1, false, 2, undefined, '', 3, null];
+// filterArray([0, 1, false, 2, undefined, '', 3, null]);
+
+// console.log(filterArray(items)); // [1, 2, 3]
+// console.log(filterArray([NaN, 0, 15, false, -22, '', undefined, 47, null])); // [15, -22, 47]
+
+//======================
+
+//Напишіть код, який поверне суму рядка
+// function digitSum(data){
+//     ...
+// }
+// console.log(digitSum('4367')) // 20
+// console.log(digitSum('56349')) // 27
+
+// function digitSum(data){
+//     const numbers = data.split("");
+//     let num = 0;
+//     for (let elem of numbers) {
+//          num += Number(elem);
+//     }
+//     return num;
+
+// }
+// console.log(digitSum('4367')) // 20
+// console.log(digitSum('56349')) // 27
+
+//======================
+
+// Потрібно мутувати масив так щоб залишились тільки (імена) елементи типу String.
+//0     1    2     3    4     5
+// const names = ['Anna', 3, 'Roma', 17, 25, 'Kate', 14, 'Oleg'];
+// for (let i = names.length - 1; i >= 0; i -= 1) {
+//   if (typeof names[i] !== 'string') {
+//     names.splice(i, 1);
+//   }
+// }
+// // Варіант №2
+// // for (let i = 0; i < names.length; i += 1){
+// //     if (typeof names[i] !== 'string') {
+// //         names.splice(i, 1);
+// //         i -= 1;  // зменшуємо індекс, бо змінилася індексація в масиві
+// //     }
+// // }
+// console.log(names);
+
+//======================
+
+// Створити код який буде повертати масив з жіночими іменамиі
+// const users = [
+//   'Artem',
+//   'Anna',
+//   'Larisa',
+//   'Maksim',
+//   'Svetlana',
+//   'David',
+//   'Roman',
+//   'Olga',
+// ];
+// const men = ['Artem', 'Maksim', 'David', 'Roman'];
+
+//======================
+
+// Створити код який буде повертати масив з жіночими іменами
+
+// const users = [
+//   'Artem',
+//   'Anna',
+//   'Larisa',
+//   'Maksim',
+//   'Svetlana',
+//   'David',
+//   'Roman',
+//   'Olga',
+// ];
+// const men = ['Artem', 'Maksim', 'David', 'Roman'];
+
+// const women = [];
+
+// for (let el of users) {
+//   if (!men.includes(el)) {
+//     console.log(el);
+//     women.push(el);
+//   }
+// }
+// console.log(women);
+
+//======================
+
+// //Створіть функцію, яка приймає 1 параметр
+// //1 масив імен користувачів, які поставили лайки.
+// //Функція повертає меседж, як вказано у прикладах:
+// //Використовувати switch()
+
+// function stringBuilder(users) {
+//   let message;
+//   switch (users.length) {
+//     case 0:
+//       message = 'no one likes this';
+//       break;
+//     case 1:
+//       message = `${users[0]} likes this `;
+//       break;
+//     case 2:
+//       message = `${users[0]} and ${users[1]} likes this `;
+//       break;
+//     case 3:
+//       message = `${users[0]}, ${users[1]} and ${users[2]} likes this `;
+//       break;
+//     default:
+//       message = `${users[0]}, ${users[1]} and ${
+//         users.length - 2
+//       } others like this`;
+//   }
+//   return message;
+// }
+
+// console.log(stringBuilder([])); //"no one likes this";
+// console.log(stringBuilder(['Peter'])); //"Peter likes this";
+// console.log(stringBuilder(['Max', 'Alex'])); //"Jacob and Alex like this";
+// console.log(stringBuilder(['Max', 'John', 'Mark'])); //"Max, John and Mark like this";
+// console.log(stringBuilder(['Alex', 'Jacob', 'Mark', 'Max', 'Igor', 'Leo'])); //"Alex, Jacob and 2 others like this;
+
+// function filterArray(numbers, value) {
+//   // Change code below this line
+//   const result = [];
+//   for (let i = 0; i < numbers.length; i += 1) {
+//     console.log(numbers[i]);
+//     if (numbers[i] > value) {
+//       console.log(numbers[i]);
+//       result.push(numbers[i]);
+//       console.log(result);
+//     }
+//   }
+//   // Change code above this line
+//   return result;
+// }
+
+// filterArray([1, 2, 3, 4, 5], 3);
--- js/script.js
+++ js/script.js
@@ -1,157 +1,86 @@
-// Напишіть код, який очищає масив від небажаних значень, таких як false undefined, пусті рядки, нуль, null.
-// function filterArray(arr){
-//     ...
+//======================
+//Напишіть функцію для перетворення імені в ініціали. Ви отримаєтеім’я з прізвищем розділені пробілом. В результаті це має виглядати так:
+// `Sam Harris => S.H`
+// `Patrick Feeney => P.F`
+// function abbrevName(name) {
+//   const arr = name.split(” “);
+//   return `${arr[0][0]}.${arr[1][0]}`;
 // }
-// console.log(filterArray([0, 1, false, 2, undefined, '', 3, null])) // [1, 2, 3]
-// console.log(filterArray([NaN, 0, 15, false, -22, '',undefined, 47, null])) // [15, -22, 47]
+// function abbrevName(name) {
+//   const arr = name.split(” “);
+//   return `${arr[0].slice(0, 1)}.${arr[1].slice(0, 1)}`;
+// }
+// function abbrevName(name) {
+//   const arr = name.split(” “);
+//   for (let i = 0; i < arr.length; i += 1) {
+//     arr[i] = arr[i].slice(0, 1);
+//   }
+//   return arr.join(“.”) + “.”;
+// }
 
-// false, 0, NaN, '', undefined, null;
+//======================
 
-// function filterArray(arr) {
-//   const result = [];
-//   for (let item of arr) {
-//     if (!!item) {
-//       result.push(item);
+// Порахуй скільки голосних букв у реченні.
+// function countVowel(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   const leter = str.toLowerCase().split('');
+//   // console.log(leter);
+//   let total = 0;
+
+//   for (let item of leter) {
+//     if (vowels.includes(item)) {
+//       total += 1;
 //     }
 //   }
-//   return result;
+//   return total;
+//   // console.log(total);
 // }
-// const items = [0, 1, false, 2, undefined, '', 3, null];
-// filterArray([0, 1, false, 2, undefined, '', 3, null]);
+// console.log(countVowel('HELLO WORLD')); // 3
+// console.log(countVowel('Junior Web Developer')); // 8
+// console.log(countVowel('knowlage is a power')); // 7
 
-// console.log(filterArray(items)); // [1, 2, 3]
-// console.log(filterArray([NaN, 0, 15, false, -22, '', undefined, 47, null])); // [15, -22, 47]
-
-//======================
-
-//Напишіть код, який поверне суму рядка
-// function digitSum(data){
-//     ...
-// }
-// console.log(digitSum('4367')) // 20
-// console.log(digitSum('56349')) // 27
-
-// function digitSum(data){
-//     const numbers = data.split("");
-//     let num = 0;
-//     for (let elem of numbers) {
-//          num += Number(elem);
+// Порахуй скільки голосних букв у реченні.
+// function countVowel(str) {
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   const leter = str.toLowerCase().split("");
+//   // console.log(leter);
+//   let total = 0;
+//   for (let item of leter) {
+//     if (vowels.includes(item)) {
+//       total += 1;
 //     }
-//     return num;
-
-// }
-// console.log(digitSum('4367')) // 20
-// console.log(digitSum('56349')) // 27
-
-//======================
-
-// Потрібно мутувати масив так щоб залишились тільки (імена) елементи типу String.
-//0     1    2     3    4     5
-// const names = ['Anna', 3, 'Roma', 17, 25, 'Kate', 14, 'Oleg'];
-// for (let i = names.length - 1; i >= 0; i -= 1) {
-//   if (typeof names[i] !== 'string') {
-//     names.splice(i, 1);
 //   }
+//   return total;
+// // console.log(total);
 // }
-// // Варіант №2
-// // for (let i = 0; i < names.length; i += 1){
-// //     if (typeof names[i] !== 'string') {
-// //         names.splice(i, 1);
-// //         i -= 1;  // зменшуємо індекс, бо змінилася індексація в масиві
-// //     }
-// // }
-// console.log(names);
-
-//======================
-
-// Створити код який буде повертати масив з жіночими іменамиі
-// const users = [
-//   'Artem',
-//   'Anna',
-//   'Larisa',
-//   'Maksim',
-//   'Svetlana',
-//   'David',
-//   'Roman',
-//   'Olga',
-// ];
-// const men = ['Artem', 'Maksim', 'David', 'Roman'];
-
-//======================
-
-// Створити код який буде повертати масив з жіночими іменами
-
-// const users = [
-//   'Artem',
-//   'Anna',
-//   'Larisa',
-//   'Maksim',
-//   'Svetlana',
-//   'David',
-//   'Roman',
-//   'Olga',
-// ];
-// const men = ['Artem', 'Maksim', 'David', 'Roman'];
-
-// const women = [];
-
-// for (let el of users) {
-//   if (!men.includes(el)) {
-//     console.log(el);
-//     women.push(el);
+// console.log(countVowel('HELLO WORLD')); // 3
+// console.log(countVowel('Junior Web Developer')); // 8
+// console.log(countVowel('knowlage is a power')); // 7
+// function countVowel(str) {
+//   const vowels = ['a', 'e', 'i', 'o', 'u'];
+//   const leter = str.toLowerCase();
+//   // console.log(leter);
+//   let total = 0;
+//   for (let i = 0; i < leter.length; i += 1) {
+//     if (vowels.includes(leter[i])) {
+//       total += 1;
+//     }
 //   }
-// }
-// console.log(women);
-
-//======================
-
-// //Створіть функцію, яка приймає 1 параметр
-// //1 масив імен користувачів, які поставили лайки.
-// //Функція повертає меседж, як вказано у прикладах:
-// //Використовувати switch()
-
-// function stringBuilder(users) {
-//   let message;
-//   switch (users.length) {
-//     case 0:
-//       message = 'no one likes this';
-//       break;
-//     case 1:
-//       message = `${users[0]} likes this `;
-//       break;
-//     case 2:
-//       message = `${users[0]} and ${users[1]} likes this `;
-//       break;
-//     case 3:
-//       message = `${users[0]}, ${users[1]} and ${users[2]} likes this `;
-//       break;
-//     default:
-//       message = `${users[0]}, ${users[1]} and ${
-//         users.length - 2
-//       } others like this`;
-//   }
-//   return message;
+//   return total;
+//   // console.log(total);
 // }
 
-// console.log(stringBuilder([])); //"no one likes this";
-// console.log(stringBuilder(['Peter'])); //"Peter likes this";
-// console.log(stringBuilder(['Max', 'Alex'])); //"Jacob and Alex like this";
-// console.log(stringBuilder(['Max', 'John', 'Mark'])); //"Max, John and Mark like this";
-// console.log(stringBuilder(['Alex', 'Jacob', 'Mark', 'Max', 'Igor', 'Leo'])); //"Alex, Jacob and 2 others like this;
-
-function filterArray(numbers, value) {
-  // Change code below this line
-  const result = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    console.log(numbers[i]);
-    if (numbers[i] > value) {
-      console.log(numbers[i]);
-      result.push(numbers[i]);
-      console.log(result);
+const arr = [1, [5, 6], 3, 4, [7, 7, [2, 3]]];
+function getSum(arr) {
+  let total = 0;
+  for (const item of arr) {
+    if (typeof item === 'number') {
+      total += item;
+    } else {
+      total += getSum(item);
     }
   }
-  // Change code above this line
-  return result;
+  return total;
 }
 
-filterArray([1, 2, 3, 4, 5], 3);
+console.log(getSum(arr));

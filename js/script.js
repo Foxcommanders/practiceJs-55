b23adcb646d0e51f157cca36c2a18099286ebd91
Foxcommanders <131612658+Foxcommanders@users.noreply.github.com>
2023-06-13 16:27:05 +0300

    13.06

--- js/script.js
+++ js/script.js
@@ -1,63 +1,67 @@
-const contentElem = document.querySelector('.content');
-const btnRestart = document.querySelector('.button');
+const elements = {
+  weekDay: document.querySelector('.date-day'),
+  monthDay: document.querySelector('.date'),
+  month: document.querySelector('.date-month'),
+  year: document.querySelector('.date-year'),
+  clock: document.querySelector('.digital-clock'),
+  arrowSeconds: document.querySelector('.clock-seconds__arrow'),
+  arrowMin: document.querySelector('.clock-minutes__arrow'),
+  arrowHour: document.querySelector('.clock-hours__arrow'),
+};
 
-btnRestart.addEventListener('click', handlerRestart);
-contentElem.addEventListener('click', handlerClick);
+const namesOfDay = [
+  'Неділя',
+  'Понеділок',
+  'Вівторок',
+  'Середа',
+  'Четвер',
+  'Пятниця',
+  'Субота',
+];
 
-const KEY_LS_X = 'History X';
-const KEY_LS_O = 'History O';
-const KEY_LS_CURRENT_PLAYER = 'Current Player';
+const namesOfMonth = [
+  'Січень',
+  'Лютий',
+  'Березень',
+  'Квітень',
+  'Травень',
+  'Червень',
+  'Липень',
+  'Серпень',
+  'Вересень',
+  'Жовтень',
+  'Листопад',
+  'Грудень',
+];
 
-const historyX = JSON.parse(localStorage.getItem(KEY_LS_X)) ?? [];
-const historyO = JSON.parse(localStorage.getItem(KEY_LS_O)) ?? [];
-let player = localStorage.getItem(KEY_LS_CURRENT_PLAYER) ?? 'X';
+setInterval(() => {
+  const date = new Date();
+  const todayDate = date.getDate();
+  const currday = date.getDay();
+  const currMonth = date.getMonth();
+  const currentYear = date.getFullYear();
 
-function createMarkup() {
-  let markup = '';
-  for (let i = 1; i <= 9; i += 1) {
-    markup += `<div class="item" data-id="${i}"></div>`;
-  }
-  contentElem.innerHTML = markup;
-  // contentElem.insertAdjacentHTML("afterbegin", markup);
-}
-createMarkup();
+  elements.weekDay.textContent = namesOfDay[currday];
+  elements.monthDay.textContent = todayDate;
+  elements.month.textContent = namesOfMonth[currMonth];
+  elements.year.textContent = currentYear;
 
-function continueGame() {
-  for (const item of contentElem.children) {
-    if (historyX.includes(item.dataset.id)) {
-      item.textContent = 'X';
-    } else if (historyO.includes(item.dataset.id)) {
-      item.textContent = 'O';
-    }
-  }
-}
-continueGame();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
 
-function handlerClick(evt) {
-  if (evt.target === evt.currentTarget || evt.target.textContent) {
-    //!evt.target.classList.contains("item")
-    return;
-  }
-  evt.target.textContent = player;
-  const id = evt.target.dataset.id;
-  if (player === 'X') {
-    historyX.push(id); // подія Х
-    localStorage.setItem(KEY_LS_X, JSON.stringify(historyX));
-  } else {
-    historyO.push(id); // подія О
-    localStorage.setItem(KEY_LS_O, JSON.stringify(historyO));
-  }
+  const clock = `${hours.toString().padStart(2, '0')} : ${minutes
+    .toString()
+    .padStart(2, '0')} : ${seconds.toString().padStart(2, '0')}`;
 
-  player = player === 'X' ? 'O' : 'X';
-  localStorage.setItem(KEY_LS_CURRENT_PLAYER, player);
-}
+  elements.clock.textContent = clock;
 
-function handlerRestart() {
-  createMarkup();
-  player = 'X';
-  localStorage.removeItem(KEY_LS_X);
-  localStorage.removeItem(KEY_LS_O);
-  localStorage.removeItem(KEY_LS_CURRENT_PLAYER);
-  historyX.splice(0, historyX.length);
-  historyO.splice(0, historyO.length);
-}
+  const secondDeg = (360 / 60) * seconds;
+  elements.arrowSeconds.style.transform = `rotate(${secondDeg}deg)`;
+
+  const minutDeg = (360 / 60) * minutes;
+  elements.arrowMin.style.transform = `rotate(${minutDeg}deg)`;
+
+  const hoursDeg = (360 / 12) * hours + minutes * 0.33;
+  elements.arrowHour.style.transform = `rotate(${hoursDeg}deg)`;
+}, 1000);
